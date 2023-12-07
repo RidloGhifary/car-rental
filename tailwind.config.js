b059ecf9115eb376598e6e6438afd9fcb3f4b874
Ridlo Achmad Ghifary <ridloghfry@gmail.com>
2023-12-07 18:33:31 +0700

    update setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        orange: "#f47920",
+        black: "#222222"
+      }
     },
   }
 }
